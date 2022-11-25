1515f881060724179db8b477cc81d9edeb4abbab
Mounika09-web <mounika.rangu09@gmail.com>
2022-11-25 15:19:35 +0530

    2nd commit

--- src/test/java/com/qa/testscripts/TC_ProductPage_001.java
+++ src/test/java/com/qa/testscripts/TC_ProductPage_001.java
@@ -2,4 +2,8 @@ package com.qa.testscripts;
 
 public class TC_ProductPage_001 {
 
+	public static void main() {
+		
+		 System.out.println("hello mounika");
+	}
 }
---
+++ src/test/java/com/qa/testscripts/TC_testing001.java
@@ -0,0 +1,11 @@
+package com.qa.testscripts;
+
+public class TC_testing001 {
+ 
+	public static void main() {
+		
+	
+	
+ System.out.println("hello world");
+}
+}
